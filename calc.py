6dff5670620a16e24aabb75a42c592967540c917
xchristina04 <vy.xchristina@gmail.com>
2024-02-05 16:20:25 -0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,19 @@
+num1 = int(input("Enter a number: "))
+op = input("Enter an operator: ")
+num2 = int(input("Enter another number: "))
 
+if op == "+":
+	total = num1 + num2
+	print(f"{num1} + {num2} = {total}")
+elif op == "-":
+	print(f"{num1} - {num2} = {num1 - num2}")
+elif op == "*":
+	print(f"{num1} * {num2} = {num1 * num2}")
+elif op == "/":
+	print(f"{num1} / {num2} = {num1 / num2}")
+elif op == "**":
+	print(f"{num1} ** {num2} = {num1 ** num2}")
+elif op =="//":
+	print(f"{num1} // {num2} = {num1 // num2}")
+else:
+	print(f"{num1} % {num2} = {num1 % num2}")	
